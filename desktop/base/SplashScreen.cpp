4905278d1e890e489b4249477c185f4783e45126
Judo-Meschede <Ralf-Paul@Hotmail.de>
2026-09-22 23:36:40 +0200

    Show version 0.2.2

--- desktop/base/SplashScreen.cpp
+++ desktop/base/SplashScreen.cpp
@@ -33,7 +33,7 @@ bool FetchSnapshotWindows(QByteArray& payload, QString& error)
     payload.clear();
     error.clear();
 
-    HINTERNET session = WinHttpOpen(L"Ipponboard-Meschede/0.2.1",
+    HINTERNET session = WinHttpOpen(L"Ipponboard-Meschede/0.2.2",
                                     WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
                                     WINHTTP_NO_PROXY_NAME,
                                     WINHTTP_NO_PROXY_BYPASS, 0);
